56fdc9833c25375b8c287c5cb63b6d242f4bf3cd
Gabriel Oliveira <gabriel.oliveira@pinkapp.com>
2024-10-28 22:31:22 -0300

    chore(setup-project): initial commit with configs and setup initial

---
+++ backend/src/test/java/com/utfpr/bytekids/BytekidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.bytekids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytekidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
