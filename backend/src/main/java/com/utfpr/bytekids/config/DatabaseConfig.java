0be7228db50666a9258015626df2b7e94d7c6a8d
patrikmelodias <patrikmelod@gmail.com>
2024-12-01 17:02:34 -0300

    Retirada de imports que não estavam sendo utilizados. Backend

--- backend/src/main/java/com/utfpr/bytekids/config/DatabaseConfig.java
+++ backend/src/main/java/com/utfpr/bytekids/config/DatabaseConfig.java
@@ -1,7 +1,5 @@
 package com.utfpr.bytekids.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
